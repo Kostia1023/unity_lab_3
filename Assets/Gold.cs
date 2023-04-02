71225781200c3adb6f7f125a474a3af8bf1af5b2
Kostia1023 <kostia1023@gmail.com>
2023-04-02 20:21:29 +0300

    lab 4

--- Assets/Gold.cs
+++ Assets/Gold.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Gold : MonoBehaviour
-{    private void OnCollisionEnter(Collision collision)
+{
+    public GameObject menuObject;
+    private Menu Menu;
+
+    public void Start()
+    {
+        Menu = menuObject.GetComponent<Menu>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+           Menu.AddGold();
+
             print("you take 1 gold");
             this.gameObject.SetActive(false);
         }
